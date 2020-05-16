a60ac726ee349fdcea7a676939c38968718d6d45
Priansh Shah <shah@priansh.com>
2020-05-15 23:35:22 -0400

    add random to utils

--- utils.js
+++ utils.js
@@ -1,1 +1,3 @@
-Logger = tag => (..._) => console.log(...[`%c[Ad Unblock 🐱‍👤 || ${tag}]`, "background-color: #ff99ff; color: #000;"],..._)
+const Logger = tag => (..._) => console.log(...[`%c[Ad Unblock 🐱‍👤 || ${tag}]`, "background-color: #ff99ff; color: #000;"],..._)
+
+const Random = (min, max) => () => Math.random() * (max - min + 1) + min

4cbbce1a53fae8bcca404fa13ea86cf27633dfda
Priansh Shah <shah@priansh.com>
2020-05-15 22:27:26 -0400

    finish adding parameters to blocker

--- blocker.js
+++ blocker.js
@@ -3,14 +3,21 @@
     const log = Logger("Blocker")
     log("Initializing.")
 
-    const urls = ["*://*.helloaiko.com/*"]
-    // explicitly block everything except main_frame :)
-    const types = ['sub_frame', "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
+    // CONFIG
+    const {
+        urls, types, block, allow
+    } = {
+        urls: ["*://*.helloaiko.com/*"],
+        // explicitly block everything except main_frame :)
+        types: ['sub_frame', "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"],
+        block: { cancel: true },
+        allow: { cancel: false },
+    }
 
     chrome.webRequest.onBeforeRequest.addListener(
         // second type check in case the browser forwards the wrong request
         ({url, type}) => (log("Blocked", url), type == "main_frame" ? allow : block),
-        {urls,},
+        {urls, types},
         ["blocking"]
     )
 

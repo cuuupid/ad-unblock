ce2cb0d0aa0c6c29876efb0bed29cb6b3bd6c902
Priansh Shah <shah@priansh.com>
2020-05-15 22:22:25 -0400

    more explicit blocking, fix typo, account for ms edge

--- blocker.js
+++ blocker.js
@@ -3,12 +3,14 @@
     const log = Logger("Blocker")
     log("Initializing.")
 
-    const urls = ["*://*.helloaiko.com/"]
+    const urls = ["*://*.helloaiko.com/*"]
+    // explicitly block everything except main_frame :)
     const types = ['sub_frame', "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
 
     chrome.webRequest.onBeforeRequest.addListener(
-        ({url,}) => (log("Blocked", url), { cancel: true }),
-        {urls, types},
+        // second type check in case the browser forwards the wrong request
+        ({url, type}) => (log("Blocked", url), type == "main_frame" ? allow : block),
+        {urls,},
         ["blocking"]
     )
 

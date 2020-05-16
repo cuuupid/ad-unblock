0b0284bfd78bbdbe51b4d75aeb86e998732ed756
Priansh Shah <shah@priansh.com>
2020-05-15 21:48:07 -0400

    setup basic blocking

--- blocker.js
+++ blocker.js
@@ -3,4 +3,12 @@
     const log = Logger("Blocker")
     log("Initializing.")
 
+    const block = {cancel: true}
+    const urls = ["*://*.helloaiko.com/"]
+    chrome.webRequest.onBeforeRequest.addListener(
+        (({url,}) => log("Blocked", url), block),
+        {urls,},
+        ["blocking"]
+    )
+
 })()

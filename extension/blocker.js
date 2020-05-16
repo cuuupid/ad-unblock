a1cb12e16b6b2004f51a8d4e578770639d4f7166
Priansh Shah <shah@priansh.com>
2020-05-15 23:51:38 -0400

    behaviour should be that of a whitelist

--- extension/blocker.js
+++ extension/blocker.js
@@ -7,16 +7,25 @@
     const {
         urls, types, block, allow
     } = {
-        urls: ["*://*.helloaiko.com/*"],
+        urls: ["<all_urls>"],
         // explicitly block everything except main_frame :)
         types: ['sub_frame', "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"],
         block: { cancel: true },
         allow: { cancel: false },
     }
 
+    const whitelist = [
+        /^[^:]*:(?:\/\/)?(?:[^\/]*\.)?helloaiko.com\/.*$/
+    ]
+    const isAllowed = url => {
+        for (let domain of whitelist)
+            if (domain.test(url)) return true;
+        return false
+    }
+
     chrome.webRequest.onBeforeRequest.addListener(
         // second type check in case the browser forwards the wrong request
-        ({url, type}) => (log("Blocked", url), type == "main_frame" ? allow : block),
+        ({url, type}) => (log("Blocked", url), (type == "main_frame" || isAllowed(url)) ? allow : block),
         {urls, types},
         ["blocking"]
     )

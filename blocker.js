c1fea581f4a3b0be3b2387fc6c5842d2aa4dc4f0
Priansh Shah <shah@priansh.com>
2020-05-15 22:13:17 -0400

    refactor, don't block main frames

--- blocker.js
+++ blocker.js
@@ -3,11 +3,12 @@
     const log = Logger("Blocker")
     log("Initializing.")
 
-    const block = {cancel: true}
     const urls = ["*://*.helloaiko.com/"]
+    const types = ['sub_frame', "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
+
     chrome.webRequest.onBeforeRequest.addListener(
-        (({url,}) => log("Blocked", url), block),
-        {urls,},
+        ({url,}) => (log("Blocked", url), { cancel: true }),
+        {urls, types},
         ["blocking"]
     )
 
